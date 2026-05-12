db774b1aa22c16095628eac1e9eadae5126a5543
angeli0283 <angeliyasmim@gmail.com>
2026-05-11 21:51:54 -0300

    atualização 1

---
+++ assets/exercícios/assets/js/exercicio-01-seletores.js
@@ -0,0 +1,7 @@
+function exercicio01CompararDoisNumeros(){
+    const campoNumero = document.getElementById("numero1");
+    const numero1 = parseInt(campoNumero.value);
+
+    const campoNumero = document.getElementById("numero2");
+    const numero2 = parseInt(campoNumero.value);
+}
